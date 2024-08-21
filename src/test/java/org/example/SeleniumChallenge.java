9cefaf65ebdcd01ad9f967e4243ca38b1f3ab136
prayashsdet <94106123+prayashsdet@users.noreply.github.com>
2024-08-21 18:48:40 +0530

    Update SeleniumChallenge.java

--- src/test/java/org/example/SeleniumChallenge.java
+++ src/test/java/org/example/SeleniumChallenge.java
@@ -23,10 +23,9 @@ public class SeleniumChallenge {
   public void testInit() {
 
 
-    // Set the path for the ChromeDriver (Not needed if WebDriverManager is used)
-    //    System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+    
     WebDriver driver = new ChromeDriver();
-    // Open Myntra website
+   
 
     driver.get("https://www.amazon.in/");
 
@@ -36,15 +35,7 @@ public class SeleniumChallenge {
 
     wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("(//div[@class='s-suggestion-container'])[1]")));
     driver.findElement(By.xpath("(//div[@class='s-suggestion-container'])[1]")).click();
- //List<WebElement> options = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//ul[@class='desktop-group']//li")));
-//
-//    for (WebElement e : options) {
-//      System.out.println(e.getText());
-//
-//    }
-//    if (options.size() > 3) {
-//      options.get(2).click();
-//    }
+
 
     List<ProductClass> productList = new ArrayList<>();
 
@@ -71,7 +62,7 @@ public class SeleniumChallenge {
         productList.add(new ProductClass(productNames.get(i).getText(), originalPrice));
 
 
-//    homepage = PageFactory.initElements(driver, HomePage.class);
+
 
     }
     Collections.sort(productList);
@@ -84,8 +75,4 @@ public class SeleniumChallenge {
 
 
 
-//  @Test
-//  public void testHomePageHasAHeader() {
-//    Assert.assertFalse("".equals(homepage.header.getText()));
-//  }
-//}
+
